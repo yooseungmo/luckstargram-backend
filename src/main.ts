28996cec68dc4fa27353185a19e60e52310706fc
Blanc <topblade6@naver.com>
2025-05-10 19:49:29 +0900

    feat: Swagger 및 ValidationPipe 설정
    - SwaggerModule 설정 추가
    - 글로벌 ValidationPipe 등록

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { blancLogger, BlancLoggerMiddleware } from 'blanc-logger';
 import { AppModule } from './app.module';
 
@@ -7,6 +9,30 @@ async function bootstrap() {
     logger: blancLogger,
   });
 
+  const config = new DocumentBuilder()
+    .setTitle('luckstargram-backend')
+    .setDescription('luckstargram-backend')
+    .setVersion('1.0.0')
+    .addBasicAuth()
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.use(new BlancLoggerMiddleware().use);
 
   await app.listen(process.env.PORT ?? 3000);
